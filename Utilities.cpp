1465c12827fd30d94cb72174a24e94444ac3090a
Austin Gladfelter <agladfelter@mail.bradley.edu>
2018-02-15 18:47:49 -0600

    Add files via upload

---
+++ Utilities.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <random>
+#include <string>
+
+#include "Utilities.h"
+
+std::default_random_engine         Utilities::_generator{ 0 };
+std::uniform_int_distribution<int> Utilities::_distribution{ 0, 100000 }; // Consistent seed, in range [0, 100000];
